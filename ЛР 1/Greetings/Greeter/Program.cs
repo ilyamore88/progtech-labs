ce529b5135f5a0322d4b259b6ba6fc5e133900da
ilyamore88 <themorozik@gmail.com>
2019-02-16 11:26:45 +0300

    done first exercise

---
+++ ЛР 1/Greetings/Greeter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greetings
+{
+    class Greeter
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            Console.WriteLine("Please enter your name");
+            myName = Console.ReadLine();
+            Console.WriteLine("Hello, {0}", myName);
+        }
+    }
+}
